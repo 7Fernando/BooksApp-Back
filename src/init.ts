f2159eedc38bd0d43e62c18c3eeacf3a2ead3337
Alejandro Turrisi <dip.aturrisi@gmail.com>
2022-04-07 15:16:32 -0300

    fetching api

--- src/init.ts
+++ src/init.ts
@@ -1,35 +1,87 @@
 import axios from "axios"
-import { Language, Topic, Book, Author} from "@prisma/client"
+import { Language, Topic, Book, Author, PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient();
 
-const getInfo = async(prop:string, prop2:string="")=>{
-    const {data} = await axios.get("https://gutendex.com/books/")
-    let result:string[] = []
-    prop2 === ""?(
-    data.results.forEach((elemento:any)=>{
-        prop === "authors"? (elemento.authors.map((elemento:{name:string})=>{
-             result.push(elemento.name)
-             result = [...new Set(result)]
-         })):
-        elemento[prop].map((elemento:string)=>{
-             result.push(elemento)
-             result = [...new Set(result)]
-         })
-    })
-    ):(result = data.results.map((elemento:any)=>({title:elemento[prop], cover:elemento[prop2]["image/jpeg"]})))
+const getInfo = async (prop: string) => {
+    const { data } = await axios.get("https://gutendex.com/books/")
+    let result: string[] = []
+    prop !== "books" ? (
+        data.results.forEach((element: any) => {
+            prop === "authors" ? (element.authors.map((element: { name: string }) => {
+                result.push(element.name)
+                result = [...new Set(result)]
+            })) :
+                element[prop].map((element: string) => {
+                    result.push(element)
+                    result = [...new Set(result)]
+                })
+        })
+    ) : (result = data.results.map((element: any) => (
+        {
+            title: element.title,
+            cover: element.formats["image/jpeg"],
+            topics: element.bookshelves,
+            author: element.authors,
+            languages: element.languages
+        }
+
+    )))
     return result
 }
 
-export const init = async () =>{
-const [
-    books,
-    topics, 
-    author,
-    languages
-] = await Promise.all([
-    getInfo("title", "formats"),
-    getInfo("bookshelves"),
-    getInfo("authors"),
-    getInfo("languages")
-])
-console.log(books, topics,author,languages)
+export const init = async () => {
+    try {
+        const verify = await prisma.book.findMany({});
+        // if (!verify.length) {
+        //     const [
+        //         books,
+        //         topics,
+        //         author,
+        //         languages
+        //     ] = await Promise.all([
+        //         getInfo("books"),
+        //         getInfo("bookshelves"),
+        //         getInfo("authors"),
+        //         getInfo("languages")
+        //     ]);
+        //     books.map((book: any) => console.table(book.author))
+        //     topics.map(async (element: any) => {
+        //         await prisma.topic.create({
+        //             data: {
+        //                 name: element
+        //             }
+        //         })
+        //     });
+        //     author.map(async (element: any) => {
+        //         await prisma.author.create({
+        //             data: {
+        //                 name: element
+        //             }
+        //         })
+        //     });
+        //     languages.map(async (element: any) => {
+        //         data: {
+        //             name: element
+        //         }
+        //     });
+        //     books.map(async (element: any) => {
+        //         await prisma.book.create({
+        //             data: {
+        //                 title: element.title,
+        //                 cover: element.formats["image/jpeg"],
+        //                 topic: {
+        //                     connect: {
+        //                         name: element.topics,
+        //                     }
+        //                 }
+        //                 // topics: element.bookshelves,
+        //                 // author: element.authors,
+        //                 // languages: element.languages
+        //             }
+        //         })
+        //     });
+        // }
+    } catch (error) {
+        console.log(error)
+    }
 }
