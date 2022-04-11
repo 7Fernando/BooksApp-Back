0a0cf5f5458d39673aaf45482d6fc5efef247939
jose1up <josealcaraz022@gmail.com>
2022-04-11 10:45:39 -0300

    database loaded

---
+++ src/sedders/books.ts
@@ -0,0 +1,708 @@
+export const seedBooks =[
+    {
+        "title": "Frankenstein; Or, The Modern Prometheus",
+        "cover": "https://www.gutenberg.org/cache/epub/84/pg84.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Gothic Fiction"
+            },
+            {
+                "name": "Movie Books"
+            },
+            {
+                "name": "Precursors of Science Fiction"
+            },
+            {
+                "name": "Science Fiction by Women"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/84.epub.images",
+        "authors": [
+            {
+                "name": "Shelley Mary Wollstonecraft"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Pride and Prejudice",
+        "cover": "https://www.gutenberg.org/cache/epub/1342/pg1342.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Best Books Ever Listings"
+            },
+            {
+                "name": "Harvard Classics"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/1342.epub.images",
+        "authors": [
+            {
+                "name": "Austen Jane"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Alice's Adventures in Wonderland",
+        "cover": "https://www.gutenberg.org/cache/epub/11/pg11.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Children's Literature"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/11.epub.images",
+        "authors": [
+            {
+                "name": "Carroll Lewis"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "The Great Gatsby",
+        "cover": "https://www.gutenberg.org/cache/epub/64317/pg64317.cover.medium.jpg",
+        "topics": [],
+        "epub": "https://www.gutenberg.org/ebooks/64317.epub.images",
+        "authors": [
+            {
+                "name": "Fitzgerald F. Scott (Francis Scott)"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "The Adventures of Sherlock Holmes",
+        "cover": "https://www.gutenberg.org/cache/epub/1661/pg1661.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Banned Books from Anne Haight's list"
+            },
+            {
+                "name": "Contemporary Reviews"
+            },
+            {
+                "name": "Detective Fiction"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/1661.epub.images",
+        "authors": [
+            {
+                "name": "Doyle Arthur Conan"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "A Tale of Two Cities",
+        "cover": "https://www.gutenberg.org/cache/epub/98/pg98.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Historical Fiction"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/98.epub.images",
+        "authors": [
+            {
+                "name": "Dickens Charles"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Simple Sabotage Field Manual",
+        "cover": "https://www.gutenberg.org/cache/epub/26184/pg26184.cover.medium.jpg",
+        "topics": [],
+        "epub": "https://www.gutenberg.org/ebooks/26184.epub.images",
+        "authors": [
+            {
+                "name": "United States. Office of Strategic Services"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "The Picture of Dorian Gray",
+        "cover": "https://www.gutenberg.org/cache/epub/174/pg174.cover.small.jpg",
+        "topics": [
+            {
+                "name": "Gothic Fiction"
+            },
+            {
+                "name": "Movie Books"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/174.epub.images",
+        "authors": [
+            {
+                "name": "Wilde Oscar"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "A Doll's House : a play",
+        "cover": "https://www.gutenberg.org/cache/epub/2542/pg2542.cover.small.jpg",
+        "topics": [
+            {
+                "name": "Best Books Ever Listings"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/2542.epub.images",
+        "authors": [
+            {
+                "name": "Ibsen Henrik"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Moby Dick; Or, The Whale",
+        "cover": "https://www.gutenberg.org/cache/epub/2701/pg2701.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Best Books Ever Listings"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/2701.epub.images",
+        "authors": [
+            {
+                "name": "Melville Herman"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Dracula",
+        "cover": "https://www.gutenberg.org/cache/epub/345/pg345.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Gothic Fiction"
+            },
+            {
+                "name": "Horror"
+            },
+            {
+                "name": "Movie Books"
+            },
+            {
+                "name": "Mystery Fiction"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/345.epub.images",
+        "authors": [
+            {
+                "name": "Stoker Bram"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "The Scarlet Letter",
+        "cover": "https://www.gutenberg.org/cache/epub/25344/pg25344.cover.small.jpg",
+        "topics": [
+            {
+                "name": "Banned Books from Anne Haight's list"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/25344.epub.images",
+        "authors": [
+            {
+                "name": "Hawthorne Nathaniel"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "The Yellow Wallpaper",
+        "cover": "https://www.gutenberg.org/cache/epub/1952/pg1952.cover.small.jpg",
+        "topics": [
+            {
+                "name": "Gothic Fiction"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/1952.epub.images",
+        "authors": [
+            {
+                "name": "Gilman Charlotte Perkins"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "A Modest Proposal: For preventing the children of poor people in Ireland, from being a burden on their parents or country, and for making them beneficial to the publick",
+        "cover": "https://www.gutenberg.org/cache/epub/1080/pg1080.cover.medium.jpg",
+        "topics": [],
+        "epub": "https://www.gutenberg.org/ebooks/1080.epub.images",
+        "authors": [
+            {
+                "name": "Swift Jonathan"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Great Expectations",
+        "cover": "https://www.gutenberg.org/cache/epub/1400/pg1400.cover.small.jpg",
+        "topics": [
+            {
+                "name": "Best Books Ever Listings"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/1400.epub.images",
+        "authors": [
+            {
+                "name": "Dickens Charles"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Jane Eyre: An Autobiography",
+        "cover": "https://www.gutenberg.org/cache/epub/1260/pg1260.cover.medium.jpg",
+        "topics": [],
+        "epub": "https://www.gutenberg.org/ebooks/1260.epub.images",
+        "authors": [
+            {
+                "name": "Brontë Charlotte"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "The Strange Case of Dr. Jekyll and Mr. Hyde",
+        "cover": "https://www.gutenberg.org/cache/epub/43/pg43.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Horror"
+            },
+            {
+                "name": "Movie Books"
+            },
+            {
+                "name": "Precursors of Science Fiction"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/43.epub.images",
+        "authors": [
+            {
+                "name": "Stevenson Robert Louis"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Heart of Darkness",
+        "cover": "https://www.gutenberg.org/cache/epub/219/pg219.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Best Books Ever Listings"
+            },
+            {
+                "name": "Movie Books"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/219.epub.images",
+        "authors": [
+            {
+                "name": "Conrad Joseph"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "The Importance of Being Earnest: A Trivial Comedy for Serious People",
+        "cover": "https://www.gutenberg.org/cache/epub/844/pg844.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Plays"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/844.epub.images",
+        "authors": [
+            {
+                "name": "Wilde Oscar"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "The Prince",
+        "cover": "https://www.gutenberg.org/cache/epub/1232/pg1232.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Banned Books from Anne Haight's list"
+            },
+            {
+                "name": "Harvard Classics"
+            },
+            {
+                "name": "Philosophy"
+            },
+            {
+                "name": "Politics"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/1232.epub.images",
+        "authors": [
+            {
+                "name": "Machiavelli Niccolò"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Metamorphosis",
+        "cover": "https://www.gutenberg.org/cache/epub/5200/pg5200.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Horror"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/5200.epub.images",
+        "authors": [
+            {
+                "name": "Kafka Franz"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Adventures of Huckleberry Finn",
+        "cover": "https://www.gutenberg.org/cache/epub/76/pg76.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Banned Books List from the American Library Association"
+            },
+            {
+                "name": "Banned Books from Anne Haight's list"
+            },
+            {
+                "name": "Best Books Ever Listings"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/76.epub.images",
+        "authors": [
+            {
+                "name": "Twain Mark"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Crime and Punishment",
+        "cover": "https://www.gutenberg.org/cache/epub/2554/pg2554.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Best Books Ever Listings"
+            },
+            {
+                "name": "Crime Fiction"
+            },
+            {
+                "name": "Harvard Classics"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/2554.epub.images",
+        "authors": [
+            {
+                "name": "Dostoyevsky Fyodor"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "The Souls of Black Folk",
+        "cover": "https://www.gutenberg.org/cache/epub/408/pg408.cover.small.jpg",
+        "topics": [
+            {
+                "name": "African American Writers"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/408.epub.images",
+        "authors": [
+            {
+                "name": "Du Bois W. E. B. (William Edward Burghardt)"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Ulysses",
+        "cover": "https://www.gutenberg.org/cache/epub/4300/pg4300.cover.small.jpg",
+        "topics": [
+            {
+                "name": "Banned Books List from the American Library Association"
+            },
+            {
+                "name": "Banned Books from Anne Haight's list"
+            },
+            {
+                "name": "Best Books Ever Listings"
+            },
+            {
+                "name": "Erotic Fiction"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/4300.epub.images",
+        "authors": [
+            {
+                "name": "Joyce James"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "War and Peace",
+        "cover": "https://www.gutenberg.org/cache/epub/2600/pg2600.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Best Books Ever Listings"
+            },
+            {
+                "name": "Historical Fiction"
+            },
+            {
+                "name": "Movie Books"
+            },
+            {
+                "name": "Napoleonic(Bookshelf)"
+            },
+            {
+                "name": "Opera"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/2600.epub.images",
+        "authors": [
+            {
+                "name": "Tolstoy Leo, graf"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Grimms' Fairy Tales",
+        "cover": "https://www.gutenberg.org/cache/epub/2591/pg2591.cover.medium.jpg",
+        "topics": [],
+        "epub": "https://www.gutenberg.org/ebooks/2591.epub.images",
+        "authors": [
+            {
+                "name": "Grimm Wilhelm"
+            },
+            {
+                "name": "Grimm Jacob"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "A Christmas Carol in Prose; Being a Ghost Story of Christmas",
+        "cover": "https://www.gutenberg.org/cache/epub/46/pg46.cover.small.jpg",
+        "topics": [
+            {
+                "name": "Children's Literature"
+            },
+            {
+                "name": "Christmas"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/46.epub.images",
+        "authors": [
+            {
+                "name": "Dickens Charles"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "The Iliad",
+        "cover": "https://www.gutenberg.org/cache/epub/6130/pg6130.cover.medium.jpg",
+        "topics": [
+            {
+                "name": "Classical Antiquity"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/6130.epub.images",
+        "authors": [
+            {
+                "name": "Homer"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Walden, and On The Duty Of Civil Disobedience",
+        "cover": "https://www.gutenberg.org/cache/epub/205/pg205.cover.medium.jpg",
+        "topics": [],
+        "epub": "https://www.gutenberg.org/ebooks/205.epub.images",
+        "authors": [
+            {
+                "name": "Thoreau Henry David"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "Little Women",
+        "cover": "https://www.gutenberg.org/cache/epub/514/pg514.cover.small.jpg",
+        "topics": [
+            {
+                "name": "Children's Literature"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/514.epub.images",
+        "authors": [
+            {
+                "name": "Alcott Louisa May"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    },
+    {
+        "title": "The Wonderful Wizard of Oz",
+        "cover": "https://www.gutenberg.org/cache/epub/55/pg55.cover.small.jpg",
+        "topics": [
+            {
+                "name": "Children's Literature"
+            },
+            {
+                "name": "Fantasy"
+            }
+        ],
+        "epub": "https://www.gutenberg.org/ebooks/55.epub.images",
+        "authors": [
+            {
+                "name": "Baum L. Frank (Lyman Frank)"
+            }
+        ],
+        "languages": [
+            {
+                "name": "en"
+            }
+        ]
+    }
+]
