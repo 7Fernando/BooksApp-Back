256b254969fb40c424e1fe2d3b3abfc2ecf9ca4e
Jose Urbani <josean.urbani@gmail.com>
2022-04-18 19:10:45 -0300

    user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 import books from './books'
 import author from './author'
 import topic from './topic'
+import user from "./users"
+
 const router = Router();
 
 router.use('/books', books);
 router.use('/author', author);
-router.use('/topic',topic)
+router.use('/topic',topic);
+router.use('/users', user)
 
 export default router;
