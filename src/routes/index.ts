4cb794777389346303d090769a7540047752dd73
Lucia Gigena <73915741+gigenaucian@users.noreply.github.com>
2022-04-19 17:17:42 -0300

    route Favorite

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import author from './author'
 import topic from './topic'
 import  language from './languege'
 import user from "./users"
-
+import  favorite  from "./favorite"
 const router = Router();
 
 router.use('/books', books);
@@ -12,6 +12,7 @@ router.use('/author', author);
 router.use('/topic',topic);
 router.use('/language', language)
 router.use('/users', user)
+router.use('/favorites', favorite)
 
 
 export default router;
