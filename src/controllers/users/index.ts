8b1b9bcc9982fbd432ce0bdb8902b5866d40a3d2
Lucia Gigena <73915741+gigenaucian@users.noreply.github.com>
2022-04-22 17:45:38 -0300

    ruteo admin

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -7,7 +7,7 @@ export const getUser = async(req: Request, res: Response ) => {
     try {
         const allUser = await prisma.user.findMany({
             include:{
-                favorite: true
+                favorite: true,
             }
         })
         allUser.length !==0 ? res.status(200).send(allUser) : res.status(404).send({msg:"User not found"})
@@ -16,6 +16,25 @@ export const getUser = async(req: Request, res: Response ) => {
         console.error(error)
     }
 }
+
+export const getUserById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const user = await prisma.user.findUnique({
+            where: {
+                id: Number(id)
+            },
+            include:{
+                favorite: true,
+            }
+        })
+        user ? res.status(200).send(user) : res.status(404).send({msg:"User not found"})
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
 
 export const postUser = async (req: Request, res: Response) => { 
     try {
@@ -28,29 +47,38 @@ export const postUser = async (req: Request, res: Response) => {
 }
 
 
-// export const getFavoriteBooks = async (userId: any) => {
-//     try {
-//       const bookId = await prisma.user.findUnique({
-//         where: {
-//           userId 
-//         },
-//         select: {
-//           favorite: true,
-//         },
-//       });
-  
-//       const favorite = bookId?.favorite.map((e) => e);
-  
-//       const allBooks = await prisma.book.findMany({
-//         where: {
-//           id: {
-//             in: favorite,
-//           },
-//         },
-//       });
-  
-//       return allBooks;
-//     } catch (error) {
-//       return null;
-//     }
-//   };
+export const modifyUser = async (req: Request, res: Response) =>{
+    try{
+        const { id , role } = req.body
+        const updateUser = await prisma.user.update({
+            where:{
+                id: Number(id)
+            },
+            data:{
+                role: role
+            }
+        })
+        updateUser? res.status(200).send(updateUser) : res.status(400).send('Not update')
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+
+
+
+export const deleteUser = async (req: Request, res: Response) =>{
+    try{
+        const id: any = req.params.id
+        const findUser = await prisma.user.delete({
+            where:{
+                id: Number(id)
+            }
+        })
+      findUser?   res.status(200).send('Usuario Borrado') : res.status(400).send('No se pudo eliminar')
+    }catch(error){
+        console.log(error)
+    }
+}
